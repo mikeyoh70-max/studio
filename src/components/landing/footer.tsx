18ac465feb6bc4d90d8cf735e0cdee49fbd66ead
Stoklaginih <stoklaginih17@gmail.com>
2026-01-15 05:13:31 +0000

    yang alfamart ganti dengan bank jago bro

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -8,7 +8,7 @@ const paymentLogos = [
   { src: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Logo_QRIS.svg/2560px-Logo_QRIS.svg.png", alt: "QRIS" },
   { src: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Logo_dana_blue.svg/2560px-Logo_dana_blue.svg.png", alt: "Dana" },
   { src: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Logo_ovo_purple.svg/2560px-Logo_ovo_purple.svg.png", alt: "OVO" },
-  { src: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Alfamart_logo.svg/1200px-Alfamart_logo.svg.png", alt: "Alfamart" },
+  { src: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Logo_Jago_-_Blue.svg/2560px-Logo_Jago_-_Blue.svg.png", alt: "Bank Jago" },
   { src: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Logo_Indomaret.png/800px-Logo_Indomaret.png", alt: "Indomaret" },
 ];
 
