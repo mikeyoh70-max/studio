336844d0f653fa126414a2190f80604f92ae658d
Stoklaginih <stoklaginih17@gmail.com>
2026-05-17 09:25:59 +0000

    kenapa gua ga bisa langsung ? apakah harus git add dulu atau apa

--- next.config.ts
+++ next.config.ts
@@ -25,10 +25,6 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'upload.wikimedia.org',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
       }
     ],
   },
