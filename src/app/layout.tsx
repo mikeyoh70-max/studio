e768dad1f6025356adbe25442fbb2e93a8a39b6f
Stoklaginih <stoklaginih17@gmail.com>
2026-05-17 09:19:27 +0000

    salah ni gua

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,6 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        {/* Google tag (gtag.js) */}
         <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=AW-18164855355"
